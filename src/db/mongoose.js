c91f6f3d13675a21d6f4b523121e1ec9430188a0
tarunraghav226 <tarun.raghav226@gmail.com>
2022-11-27 23:04:35 +0530

    Added mongoose model and validators

---
+++ src/db/mongoose.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+
+const connectionURL = "mongodb://127.0.0.1:27017"
+
+mongoose.connect(connectionURL+"/task-manager-api", {
+    useNewUrlParser: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        validate(value){
+            if(value < 0){
+                throw new Error("Age must be a positive number")
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: "Tarun",
+    age: -22
+})
+
+me.save().then(()=>{
+    console.log(me)
+}).catch((error)=>{
+    console.log(error)
+})
