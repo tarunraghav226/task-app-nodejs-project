db6d2fb97dd7a4db078d946ce787e5528d241d9a
tarunraghav226 <tarun.raghav226@gmail.com>
2022-12-11 22:38:55 +0530

    Added apis to read data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { ObjectId } = require("mongodb")
 require('./db/mongoose')
 
 const User = require("./models/User")
@@ -18,9 +19,44 @@ app.post("/users", (req, res)=>{
             data: user
         })
     }).catch(()=>{
+        return res
+        .send({
+            status: "failure",
+            data: []
+        })
+    })
+})
+
+app.get("/users", (req, res) =>{
+    User.find({})
+            .then((users) => {
+                return res.send(users)
+            })
+            .catch((err)=>{
+                return res.send({
+                    status: "failure",
+                    data: "fail"
+                })
+            })
+})
+
+app.get("/users/:id", (req, res) =>{
+    const _id = req.params.id
+    User.findOne({_id})
+    .then((user) => {
+        if(!user){
+            return res.status(404).send({
+                status: "failure",
+                data: "User not found"
+            })
+        }
+        return res.send(user)
+    })
+    .catch((err)=>{
+        console.log(err)
         return res.send({
             status: "failure",
-            data: "fail"
+            data: null
         })
     })
 })
