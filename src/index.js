74f517f8bf21a692b095438e39b563916e2f56bc
tarunraghav226 <tarun.raghav226@gmail.com>
2023-02-12 00:45:09 +0530

    Added route handling

--- src/index.js
+++ src/index.js
@@ -1,106 +1,12 @@
 const express = require("express")
-const { ObjectId } = require("mongodb")
-require('./db/mongoose')
-
-const User = require("./models/User")
+const userRoute = require("./routes/userRoutes")
 
 const app = express()
+app.use(userRoute)
 
 app.use(express.json())
 
 const port = process.env.PORT || 3000
-
-
-app.post("/users", async (req, res)=>{
-    const user = new User(req.body)
-    try{
-        await user.save()
-        return res.send({
-            status: "success",
-            data: user
-        })
-    }catch(e){
-        return res
-        .send({
-            status: "failure",
-            data: []
-        })
-    }
-})
-
-app.get("/users", async (req, res) =>{
-    try{
-        const users = User.find({})
-        return res.send(users)
-    }catch(e){
-        return res.send({
-            status: "failure",
-            data: "fail"
-        })
-    }
-})
-
-app.get("/users/:id", async (req, res) =>{
-    const _id = req.params.id
-    try{
-        const user = await User.findOne({_id})
-        if(!user){
-            return res.status(404).send({
-                status: "failure",
-                data: "User not found"
-            })
-        }
-        return res.send(user)
-    } catch (e){
-        return res.send({
-            status: "failure",
-            data: null
-        })
-    }
-})
-
-app.patch("/users/:id", async (req, res) => {
-    try{
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-        if(!updatedUser){
-            return res.status(404).send({
-                status: "failure",
-                data: null
-            })
-        }
-
-        return res.status(200).send({
-            status: "success",
-            data: updatedUser
-        })
-    }catch(e){
-        return res.status(400).send({
-            status: "failure",
-            message: e
-        })
-    }
-})
-
-app.delete("/users/:id", async (req, res) => {
-    try{
-        const deletedUser = await User.findByIdAndDelete(req.params.id)
-        if(!deletedUser){
-            return res.status(404).send({
-                status: "failure",
-                data: null
-            })
-        }
-        return res.status(200).send({
-            status: "success",
-            message: "User deleted succesfully"
-        })
-    }catch(e){
-        return res.status(400).send({
-            status: "failure",
-            message: e
-        })
-    }
-})
 
 app.listen(port, ()=>{
         console.log(`Server is up and running on port ${port}`)
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,99 @@
+const mongoose = require("../db/mongoose")
+const express = require("express")
+const User = require("./models/User")
+const { ObjectId } = require("mongodb")
+
+const userRoute = express.Router()
+
+userRoute.post("/users", async (req, res)=>{
+    const user = new User(req.body)
+    try{
+        await user.save()
+        return res.send({
+            status: "success",
+            data: user
+        })
+    }catch(e){
+        return res
+        .send({
+            status: "failure",
+            data: []
+        })
+    }
+})
+
+userRoute.get("/users", async (req, res) =>{
+    try{
+        const users = User.find({})
+        return res.send(users)
+    }catch(e){
+        return res.send({
+            status: "failure",
+            data: "fail"
+        })
+    }
+})
+
+userRoute.get("/users/:id", async (req, res) =>{
+    const _id = req.params.id
+    try{
+        const user = await User.findOne({_id})
+        if(!user){
+            return res.status(404).send({
+                status: "failure",
+                data: "User not found"
+            })
+        }
+        return res.send(user)
+    } catch (e){
+        return res.send({
+            status: "failure",
+            data: null
+        })
+    }
+})
+
+userRoute.patch("/users/:id", async (req, res) => {
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!updatedUser){
+            return res.status(404).send({
+                status: "failure",
+                data: null
+            })
+        }
+
+        return res.status(200).send({
+            status: "success",
+            data: updatedUser
+        })
+    }catch(e){
+        return res.status(400).send({
+            status: "failure",
+            message: e
+        })
+    }
+})
+
+userRoute.delete("/users/:id", async (req, res) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.params.id)
+        if(!deletedUser){
+            return res.status(404).send({
+                status: "failure",
+                data: null
+            })
+        }
+        return res.status(200).send({
+            status: "success",
+            message: "User deleted succesfully"
+        })
+    }catch(e){
+        return res.status(400).send({
+            status: "failure",
+            message: e
+        })
+    }
+})
+
+module.exports = userRoute
