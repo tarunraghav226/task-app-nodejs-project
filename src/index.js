bab49bbce7c0d5d76de443ae72ec5624dac51b58
tarun <tarun.1@pw.live>
2023-03-25 19:56:25 +0530

    added task apis

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const userRoute = require("./routes/userRoutes")
+const taskRoute = require("./routes/taskRoutes")
 
 const app = express()
 
 app.use(express.json())
+app.use(taskRoute)
 app.use(userRoute)
 
 const port = process.env.PORT || 3000
---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const Task = require('../models/task')
+const auth = require('../middlewares/auth')
+
+const taskRoute = new express.Router()
+
+taskRoute.post('/tasks', auth, async (req, res) => {
+    const task = new Task({
+        ...req.body,
+        owner: req.user._id
+    })
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+taskRoute.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+taskRoute.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById(_id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+taskRoute.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findById(req.params.id)
+
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+taskRoute.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = taskRoute
